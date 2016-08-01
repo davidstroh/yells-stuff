1e6149723a0f8c52f3fb842989ff766aae65b69a
davidstroh <dstroh10@gmail.com>
2016-07-31 20:42:04 -0500

    structured the app. Almost ready for Heroku deployment

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 var express = require('express');
+var app = express();
+module.exports = router = express.Router();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+
+module.exports = request = require('request');
+
+var yellsBot1 = require('./routes/yellsbot1');
+
+
+//module.exports = bot = require('fancy-groupme-bot');
+
+//module.exports = bot = require('./bots/yellsbot1');
+
+var port = 5000 || process.env.PORT;
+
+app.use('/bot', router);
+
+
+app.listen(port);
+//console.log(botConfig);
+
+console.log('Magic happens on port ' + port);
---
+++ routes/yellsbot1.js
@@ -0,0 +1,47 @@
+var botConfig = { token:'3107c520384201344ec26763b4a81e13',
+                  group:'23303302',
+                  name:'yells stuff',
+                  url:'https://localhost:3000/api/rec' };
+/*var yellsBot = bot(botConfig);
+yellsBot.on('botMessage', function(bot, message) {console.log(message.name + " said " + message.text);} );
+yellsBot.message("Oh! It's the military community!");*/
+//app.use('/yellsstuff', router);
+
+router.get('/yells', function(req, res, next){
+  res.json( {'title': 'heya'});
+  next();
+});
+
+//router.get('/yells/members', function(req, res, next){
+
+  var options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/get/groups',
+	  method: 'GET',
+	  form: {'bot_id': 'a4966a5b83637f95d4d474867b', 'group': '23303302'}
+  };
+
+  body = {
+    "bot_id": "eb391451ee33af0432fbc6e0f7",
+    "group": "23303302"
+  };
+
+  request(options, function (error, response, body){
+    if (!error && response.statusCode == 200) {
+      console.log("the request happen");
+    }
+    else {
+      console.log('rejecting bad status code ' + response.statusCode);
+    }
+  });
+//});
+
+/*router.post('/yell', function(req, res, next){
+  var senderType = req.body.sender_type,
+    text = req.body.text,
+    senderName = req.body.name;
+    console.log(req.body);
+});*/
+
+
+console.log(botConfig);
