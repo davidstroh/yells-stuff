c06303fcfea789702a88f35959095b2c570aea1f
davidstroh <dstroh10@gmail.com>
2016-07-31 22:34:46 -0500

    testing

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 var express = require('express');
 var app = express();
-module.exports = router = express.Router();
+//module.exports = router = express.Router();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 
-module.exports = request = require('request');
+//module.exports = request = require('request');
 
 //var yellsBot1 = require('./routes/yellsbot1');
 
-
-//module.exports = bot = require('fancy-groupme-bot');
-
-//module.exports = bot = require('./bots/yellsbot1');
-
 var port = 5000 || process.env.PORT;
+
+app.get('/', function(req, res){
+  res.json( {'title': 'heya'} );
+})
 
 //app.use('/bot', router);
 
